ccca3b4d44e5349586ad2262851a57ef5c045219
carlosahs <tacahs12@gmail.com>
2022-04-16 14:01:07 -0500

    Call methods from turtle

--- main.py
+++ main.py
@@ -1,18 +1,5 @@
 import turtle
 
-from turtle import backward
-from turtle import begin_fill
-from turtle import bye
-from turtle import color
-from turtle import done
-from turtle import end_fill
-from turtle import forward
-from turtle import listen
-from turtle import left
-from turtle import onkey
-from turtle import pos
-from turtle import right
-
 SNAKE_COLOR = (0, 0, 0)
 SNAKE_SIZE = 0.5
 
@@ -38,17 +25,17 @@ class Square(turtle.Turtle):
 
 def move_backward() -> None:
     disable_keys()
-    backward(200)
+    turtle.backward(200)
     enable_keys()
 
 
 def move_forward() -> None:
     disable_keys()
 
-    forward(200)
-    if abs(pos()) < 1:
-        end_fill()
-        left(170)
+    turtle.forward(200)
+    if abs(turtle.pos()) < 1:
+        turtle.end_fill()
+        turtle.left(170)
         return
 
     enable_keys()
@@ -56,30 +43,30 @@ def move_forward() -> None:
 
 def move_left() -> None:
     disable_keys()
-    left(170)
+    turtle.left(170)
     enable_keys()
 
 
 def move_right() -> None:
     disable_keys()
-    right(170)
+    turtle.right(170)
     enable_keys()
 
 
 def enable_keys() -> None:
-    onkey(move_forward, 'k')
-    onkey(move_backward, 'j')
-    onkey(move_left, 'h')
-    onkey(move_right, 'l')
-    onkey(bye, 'q')
+    turtle.onkey(move_forward, 'k')
+    turtle.onkey(move_backward, 'j')
+    turtle.onkey(move_left, 'h')
+    turtle.onkey(move_right, 'l')
+    turtle.onkey(turtle.bye, 'q')
 
 
 def disable_keys() -> None:
-    onkey(None, 'k')
-    onkey(None, 'j')
-    onkey(None, 'h')
-    onkey(None, 'l')
-    onkey(None, 'q')
+    turtle.onkey(None, 'k')
+    turtle.onkey(None, 'j')
+    turtle.onkey(None, 'h')
+    turtle.onkey(None, 'l')
+    turtle.onkey(None, 'q')
 
 
 if __name__ == '__main__':
@@ -90,10 +77,10 @@ if __name__ == '__main__':
 
     sqr = Square(10, 50)
 
-    enable_keys()
-
-    color('red', 'yellow')
-    begin_fill()
-
-    listen()
-    done()
+    # enable_keys()
+    #
+    # color('red', 'yellow')
+    # begin_fill()
+    #
+    # listen()
+    turtle.done()
